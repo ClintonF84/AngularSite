4743e5bd66784201b02516c073e8fae71d16f2a5
Clinton Findlay <clintonf84@gmail.com>
2022-10-05 00:44:22 -0600

    Check in
    Update for dev build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { BlogspotComponent } from './blogspot/blogspot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    BlogspotComponent
   ],
   imports: [
     BrowserModule,
